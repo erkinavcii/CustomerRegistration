7c9705d6082820ff0fe3c257d2a3282599a3c6cc
erkinavcii <56629073+erkinavcii@users.noreply.github.com>
2023-08-25 10:14:50 +0300

    ClientInfo sınıfı OOP daha uygun olması açısından Models içine taşındı.Kullanıldığı diğer sayfalara referans verildi.

---
+++ Loggma1/Models/ClientInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Loggma1.Models
+{
+    public class ClientInfo
+    {
+        public string id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string phone { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string address { get; set; }
+
+        [Required(ErrorMessage = "IdentityNumber is required")]
+        public string IdentityNumber { get; set; }
+    }
+}
--- Loggma1/Pages/Clients/Index.cshtml.cs
+++ Loggma1/Pages/Clients/Index.cshtml.cs
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using Microsoft.AspNetCore.Http;
+using Loggma1.Models;
 
 namespace Loggma1.Pages.Clients
 {
@@ -92,25 +93,25 @@ namespace Loggma1.Pages.Clients
 
    
 
-public class ClientInfo
-    {
-        public string id { get; set; }
+//public class ClientInfo
+//    {
+//        public string id { get; set; }
 
-        [Required(ErrorMessage = "Name is required")]
-        public string name { get; set; }
+//        [Required(ErrorMessage = "Name is required")]
+//        public string name { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid email format")]
-        public string email { get; set; }
+//        [Required(ErrorMessage = "Email is required")]
+//        [EmailAddress(ErrorMessage = "Invalid email format")]
+//        public string email { get; set; }
 
-        [Required(ErrorMessage = "Phone is required")]
-        public string phone { get; set; }
+//        [Required(ErrorMessage = "Phone is required")]
+//        public string phone { get; set; }
 
-        [Required(ErrorMessage = "Address is required")]
-        public string address { get; set; }
+//        [Required(ErrorMessage = "Address is required")]
+//        public string address { get; set; }
 
-        [Required(ErrorMessage = "IdentityNumber is required")]
-        public string IdentityNumber { get; set; }
-    }
+//        [Required(ErrorMessage = "IdentityNumber is required")]
+//        public string IdentityNumber { get; set; }
+//    }
 
 }
